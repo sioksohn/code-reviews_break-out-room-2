9ce44ab5400d268f42e6af92a60985eaf021da06
Elaine <sosohn@gmail.com>
2022-12-21 12:51:53 -0800

    Add questions list to be maintainable

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 #Mad Libs Generator Project
 
 #Loop back to this point once code finishes
-# for 10 times loop 
-# initialized loop as 0, loop = 1 to loop = 0 
-# # while condition was changed from loop<9 to loop<= 10
 loop = 0 
-while (loop <= 10):
+while (loop < 10):
 #All the questions that the program asks the user
-    noun = input("Choose a noun: ")
-    plural_noun = input("Choose a plural noun: ")
-    second_noun = input("Choose a noun: ")
-    place = input("Name a place: ")
-    adjective = input("Choose an adjective (Describing word): ")
-    third_noun = input("Choose a noun: ")
+    questions = [
+        "Choose a noun: ",
+        "Choose a plural noun: ",
+        "Choose a noun: ",
+        "Name a place: ",
+        "Choose an adjective (Describing word): ",
+        "Choose a noun: "
+    ]
+    noun = input(questions[0])
+    plural_noun = input(questions[1])
+    second_noun = input(questions[2])
+    place = input(questions[3])
+    adjective = input(questions[4])
+    third_noun = input(questions[5])
 #Displays the story based on the users input
     print ("------------------------------------------")
-    print ("Be kind to your",noun,"- footed", plural_noun)
-    print ("For a duck may be somebody's", second_noun,",")  #correct bug from seond_noun to second_noun
-    print ("Be kind to your",plural_noun,"in",place)
-    print ("Where the weather is always",adjective,".")
+    print (f"Be kind to your {noun} - footed {plural_noun}")
+    print (f"For a duck may be somebody's {second_noun},")  
+    print (f"Be kind to your {plural_noun} in {place}")
+    print (f"Where the weather is always {adjective}.")
     print ()
-    print ("You may think that is this the",third_noun,",")
+    print (f"You may think that is this the{third_noun},")
     print ("Well it is.")
     print ("------------------------------------------")
 #Loop back to "loop = 1"
